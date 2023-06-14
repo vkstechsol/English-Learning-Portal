7faa00c73a2dc4a2205386907d502d52b21746c5
vkstechsol <109616740+vkstechsol@users.noreply.github.com>
2023-06-14 20:15:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,25 +71,6 @@ setInterval(changeWord1, 3000);
 
 
 
-<script type="text/javascript">
-             function googleTranslateElementInit() {
-               new google.translate.TranslateElement(
-                {pageLanguage: 'en'},
-                'google_translate_element'
-            );
-        }
-
-             </script>
- 
-             <script type="text/javascript"
-              src=
-                 "https://translate.google.com/translate_a/element.js?
-                  cb=googleTranslateElementInit">
-             </script>
-
-
-
-
 
 
 
